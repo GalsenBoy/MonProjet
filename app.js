6cae71a50fae36e14e08d9219232ddebc9e6aaaa
cissokho <bilaly.cissokho21@gmail.com>
2022-04-02 17:20:32 +0200

    Le boutton responsive à résoudre!

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ crypto.addEventListener('mouseover', function(e) {
         document.getElementById('ul_crypto').style.display = "none";
     })
 })
+
+function mouseover() {
+
+}
+
+
 const nft = document.querySelector("#nft")
 nft.addEventListener('mouseover', function(e) {
     document.getElementById('ul_nft').style.display = "block";
@@ -19,35 +25,15 @@ nft.addEventListener('mouseover', function(e) {
 })
 const metavers = document.querySelector('#metavers')
 metavers.addEventListener('mouseover', function(e) {
-        document.getElementById('ul_metavers').style.display = "block";
-        document.getElementById('ul_nft').style.display = "none";
-        document.getElementById('ul_crypto').style.display = "none";
-        body.addEventListener('click', function() {
-            document.getElementById('ul_metavers').style.display = "none";
-        })
+    document.getElementById('ul_metavers').style.display = "block";
+    document.getElementById('ul_nft').style.display = "none";
+    document.getElementById('ul_crypto').style.display = "none";
+    body.addEventListener('click', function() {
+        document.getElementById('ul_metavers').style.display = "none";
     })
-    /*
-    let ouvrir = document.querySelector('.ouvrir');
-    let liste = document.querySelector('.liste');
-    let acces = document.querySelector('.acces')
-    ouvrir.addEventListener('click', function() {
-        if (liste.style.transform == 'translateX(-100%)') {
-            liste.style.transform = 'translateX(0)';
-            liste.style.transition = 'transform 2s';
-            acces.style.transform = 'translateX(0)';
-            acces.style.transition = 'transform 2s';
+})
 
-        } else {
-            liste.style.transform = 'initial';
-            acces.style.transform = 'initial';
-        }
-    })
-    */
-    /*
-    button.addEventListener("click", function() {
-      box.classList.toggle("bg-red");
-    });
-    */
+
 
 let ouvrir = document.querySelector('.ouvrir');
 ouvrir.addEventListener('click', function() {
