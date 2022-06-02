e7bdca75ff429126b98265094a9dd14b96d88f4e
bilaly <billycsk@gmail.com>
2022-06-02 19:52:56 +0200

    Nft animation added crtypto à revoir

---
+++ js/nft.js
@@ -0,0 +1,35 @@
+const nft = ScrollReveal();
+nft.reveal('.image_nft', {
+    origin: 'left',
+    duration: 2000,
+    distance: '80px'
+})
+nft.reveal('li', {
+    origin: 'left',
+    duration: 2000,
+    distance: '80px',
+    interval: 150,
+})
+
+/****************** */
+
+nft.reveal('.definition_nft', {
+    delay: 500,
+    distance: '50px',
+    duration: 2000
+})
+
+nft.reveal('.txt', {
+    distance: '70px',
+    delay: 1000,
+    duration: 1500,
+})
+
+nft.reveal('.image-top', {
+    duration: 1000,
+    interval: 400
+})
+nft.reveal('td', {
+    duration: 1000,
+    interval: 50,
+})
