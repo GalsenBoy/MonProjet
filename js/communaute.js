43f3a5d51acc6abe7bc08524a86c9030ef048fa7
bilaly <billycsk@gmail.com>
2022-06-05 20:45:07 +0200

    Commentaire animation done

--- js/communaute.js
+++ js/communaute.js
@@ -1,4 +1,39 @@
-let base = document.querySelector('.communaute_container');
+const commu = ScrollReveal();
 
-const hauteur = base.clientHeight;
-console.log(hauteur);
+commu.reveal('.communaute_container', {
+    origin: 'left',
+    distance: '85px',
+    duration: 2000
+})
+
+commu.reveal('.avatar', {
+    origin: 'left',
+    duration: 2000,
+    delay: 700
+})
+commu.reveal('.pseudo', {
+    origin: 'left',
+    distance: '15px',
+    duration: 2000,
+    delay: 800,
+})
+commu.reveal('#date', {
+    origin: 'left',
+    distance: '15px',
+    duration: 2000,
+    delay: 900,
+})
+commu.reveal('.texte_analyse', {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 1000,
+    interval: 500,
+})
+commu.reveal('.commentaire', {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    delay: 1100,
+
+})
